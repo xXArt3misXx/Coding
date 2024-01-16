7f6a49135c0cc8df9642fe7a21ec643b83ec0dff
xXArt3misXx <156848117+xXArt3misXx@users.noreply.github.com>
2024-01-16 12:09:49 -0800

    Prompt 2 Journal 1

---
+++ Prompt2.py
@@ -0,0 +1,14 @@
+first = float(input('Input first number: '))
+second = float(input('Input second number: '))
+
+print(first+second)
+
+first = int(input('Input first integer: '))
+second = int(input('Input second integer: '))
+
+print(first-second)
+
+first = float(input('Input a number: '))
+second = int(input('Input an integer: '))
+
+print(first*second)
