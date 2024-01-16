bab3ab9707ac7bf4a2ff9ba829b61cec5af13103
xXArt3misXx <156848117+xXArt3misXx@users.noreply.github.com>
2024-01-16 12:01:48 -0800

    Prompt 1 Journal 1

---
+++ Prompt1.py
@@ -0,0 +1,9 @@
+'''Code journal #1'''
+
+#1
+fullName = 'My name is Kaitlyn Birtell.'
+pronouns = 'My pronouns are They/She.'
+movie = 'My favorite movie is The Crow.'
+food = 'My favorite food is Miso soup.'
+
+print(fullName, pronouns, movie, food)
